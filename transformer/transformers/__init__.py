9379fbd3482190c38bcaac43952177a0bd4913d7
amontane <andrew@dhcp-18-111-109-113.dyn.MIT.EDU>
2018-02-12 22:27:45 -0500

    fixed importing

--- transformer/transformers/__init__.py
+++ transformer/transformers/__init__.py
@@ -1,6 +1,16 @@
 # from DT_Transformer import DT_Transformer
 import importlib
-l = ['DT_Transformer']
+from os import listdir
+from os.path import isfile, join, realpath, dirname
+
+mypath = realpath(__file__)
+directory = dirname(mypath)
+onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
+l = []
+for file in onlyfiles:
+	if file.split('.')[1] == 'py' and file.split('.')[0] != '__init__':
+		l.append(file.split('.')[0])
+# l = ['DT_Transformer']
 prefix = 'transformers.'
 
 for transformer in l:
