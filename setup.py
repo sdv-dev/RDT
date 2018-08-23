c34760c4633b59c9a86c3bfa9eed856737146d7b
Carles Sala <c.sala.stq@gmail.com>
2018-08-23 14:16:17 +0200

    Fix requirements and version

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""The setup script."""
-
 from setuptools import setup, find_packages
 
 with open('README.md') as readme_file:
@@ -12,20 +10,32 @@ with open('HISTORY.md') as history_file:
     history = history_file.read()
 
 install_requires = [
-    'Click>=6.0',
-    'boto3==1.4.7',
     'numpy==1.13.1',
     'pandas==0.22.0',
-    'scipy==0.19.1'
+    'scipy==0.19.1',
 ]
 
-setup_requires = ['pytest-runner==4.2',]
+setup_requires = [
+    'pytest-runner>=2.11.1',
+]
 
 tests_require = [
-    'pytest==3.6.1',
     'coverage>=4.5.1',
+    'pytest>=3.4.2',
     'tox>=2.9.1',
-    'pluggy==0.6.0'
+]
+
+development_requires = [
+    'bumpversion>=0.5.3',
+    'Sphinx>=1.7.1',
+    'recommonmark>=0.4.0',
+    'sphinx_rtd_theme>=0.2.4',
+    'flake8>=3.5.0',
+    'isort>=4.3.4',
+    'autoflake>=1.1',
+    'autopep8>=1.3.5',
+    'twine>=1.10.0',
+    'wheel>=0.30.0',
 ]
 
 setup(
@@ -43,13 +53,14 @@ setup(
     description="A repository with reversible data transforms",
     extras_require={
         'test': tests_require,
+        'dev': development_requires + tests_require
     },
+    include_package_data=True,
     install_requires=install_requires,
+    keywords='rdt',
     license="MIT license",
     long_description=readme + '\n\n' + history,
     long_description_content_type='text/markdown',
-    include_package_data=True,
-    keywords='rdt',
     name='rdt',
     packages=find_packages(include=['rdt', 'rdt.*']),
     python_requires='>=3.5',
@@ -57,6 +68,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/RDT',
-    version='0.1.0',
+    version='0.1.0-dev',
     zip_safe=False,
 )
