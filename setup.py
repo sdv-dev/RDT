fa8af7205f75d1373d847ce0560c8585f62aabdb
Manuel Alvarez <manuel@pythiac.com>
2018-10-16 22:39:44 +0200

    Bump version: 0.1.1-dev → 0.1.1

--- setup.py
+++ setup.py
@@ -68,6 +68,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/RDT',
-    version='0.1.1-dev',
+    version='0.1.1',
     zip_safe=False,
 )
