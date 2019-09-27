09a0d8d5754aadc407e6b4a9332cc1955874bd26
JDTheRipperPC <jdtheripperpc@gmail.com>
2019-09-27 12:09:05 +0200

    nan and null_column settings in numerical and datetime transformers

--- rdt/transformers/datetime.py
+++ rdt/transformers/datetime.py
@@ -9,21 +9,48 @@ from rdt.transformers.base import BaseTransformer
 class DateTimeTransformer(BaseTransformer):
     """Transformer for datetime data."""
 
-    def __init__(self, datetime_format):
-        self.datetime_format = datetime_format
+    def __init__(self, settings={}):
+        self.datetime_format = settings['datetime_format']
+        self.nan = settings.get('nan', 'mean')
+        self.null_column = settings.get('null_column', True)
+
+    def _get_default(self, data):
+        if isinstance(data, np.ndarray):
+            data = pd.Series(data)
+
+        if self.nan == 'ignore':
+            return None
+
+        if self.nan == 'mean':
+            _slide = ~data.isnull()
+            return data[_slide].mean()
+
+        if self.nan == 'mode':
+            data_mode = pd.to_datetime(data).mode()
+            return data_mode[data_mode.first_valid_index()]
+
+        return self.nan
+
+    def _get_null_column(self, data):
+        vfunc = np.vectorize(lambda x: 1 if x else 0)
+        return vfunc(data)
 
     def transform(self, data):
-        if isinstance(data, pd.Series):
-            data = data.to_numpy()
+        if isinstance(data, np.ndarray):
+            data = pd.Series(data)
 
-        datetime_data = pd.to_datetime(data, format=self.datetime_format, errors='coerce')
+        extra_column = None
+        if self.null_column:
+            extra_column = self._get_null_column(data.to_numpy())
 
-        # Don't transform NaT
-        # REVIEW
-        _slice = ~datetime_data.isnull()
-        transformed = datetime_data[_slice].to_numpy().astype('int64')
+        data = pd.to_datetime(data, format=self.datetime_format, errors='coerce')
 
-        return transformed
+        default = self._get_default(data)
+        if default is not None:
+            default = pd.to_datetime(default, format=self.datetime_format, errors='coerce')
+            data = data.fillna(default)
+        
+        return data.to_numpy().astype('int64'), extra_column
 
     def _transform_to_date(self, data):
         """Transform a numeric value into str datetime format."""
--- rdt/transformers/number.py
+++ rdt/transformers/number.py
@@ -9,20 +9,52 @@ class NumericalTransformer(BaseTransformer):
 
     default = None
 
+    def __init__(self, settings={}):
+        self.nan = settings.get('nan', 'mean')
+        self.null_column = settings.get('null_column', True)
+        self.default = None
+
+    def _get_default(self, data):
+        if self.nan == 'ignore':
+            return None
+
+        if self.nan == 'mean':
+            _slide = ~data.isnull()
+            return data[_slide].mean()
+
+        if self.nan == 'mode':
+            data_mode = data.mode()
+            return data_mode[data_mode.first_valid_index()]
+
+        return self.nan
+
     def fit(self, data):
         if isinstance(data, np.ndarray):
             data = pd.Series(data)
 
         self.dtype = data.dtype
-        self.default = data.mean()
+        self.default = self._get_default(data)
+
+    def _get_null_column(self, data):
+        vfunc = np.vectorize(lambda x: 1 if x and not np.isnan(x) else 0)
+        return vfunc(data)
 
     def transform(self, data):
+        extra_column = None
+
+        if self.null_column and isinstance(data, pd.Series):
+            extra_column = self._get_null_column(data.to_numpy())
+
         if isinstance(data, np.ndarray):
+            if self.null_column:
+                extra_column = self._get_null_column(data)
             data = pd.Series(data)
 
-        transformed = data.fillna(self.default)
+        transformed = data
+        if self.default is not None:
+            transformed = data.fillna(self.default)
 
-        return transformed.to_numpy()
+        return transformed.to_numpy(), extra_column
 
     def reverse_transform(self, data):
         if not isinstance(data, np.ndarray):
