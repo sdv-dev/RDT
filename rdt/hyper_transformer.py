97ac6e4f6b0b6d7504d8dfc939769378d8b1dbec
Carles Sala <carles@pythiac.com>
2019-10-07 10:27:42 +0200

    Add automatic column type detection

--- rdt/hyper_transformer.py
+++ rdt/hyper_transformer.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 from rdt import transformers
@@ -31,6 +32,7 @@ class HyperTransformer:
             Wheter or not to handle missing values before transforming data.
             Defaults to ``True``.
     """
+    transformers = None
 
     @staticmethod
     def get_class(name):
@@ -60,15 +62,42 @@ class HyperTransformer:
 
         return transformer_class(**transformer_kwargs)
 
-    def __init__(self, column_transformers, copy=True):
-        self.transformers = {
-            column_name: self._load_transformer(transformer)
-            for column_name, transformer in column_transformers.items()
-        }
+    def __init__(self, column_transformers=None, copy=True, anonymize=None):
+        if column_transformers:
+            self.transformers = {
+                column_name: self._load_transformer(transformer)
+                for column_name, transformer in column_transformers.items()
+            }
+
         self.copy = copy
+        self.anonymize = anonymize or dict()
+
+    def _analyze(self, data):
+        column_transformers = dict()
+        for name, column in data.items():
+            if column.dtype in [np.int, np.float]:
+                transformer = transformers.NumericalTransformer()
+            elif column.dtype == np.object:
+                anonymize = self.anonymize.get(name)
+                transformer = transformers.CategoricalTransformer(anonymize=anonymize)
+            elif column.dtype == np.bool:
+                transformer = transformers.BooleanTransformer()
+            elif np.issubdtype(column.dtype, np.datetime64):
+                transformer = transformers.DatetimeTransformer()
+            else:
+                raise ValueError('Unsupported dtype: {}'.format(column.dtype))
+
+            column_transformers[name] = transformer
+
+        return column_transformers
 
     def fit(self, data):
-        for column_name, transformer in self.transformers.items():
+        if self.transformers:
+            self._transformers = self.transformers
+        else:
+            self._transformers = self._analyze(data)
+
+        for column_name, transformer in self._transformers.items():
             column = data[column_name]
             transformer.fit(column)
 
@@ -76,16 +105,16 @@ class HyperTransformer:
         if self.copy:
             data = data.copy()
 
-        for column_name, transformer in self.transformers.items():
+        for column_name, transformer in self._transformers.items():
             column = data.pop(column_name)
             transformed = transformer.transform(column)
 
             shape = transformed.shape
 
             if len(shape) == 2 and shape[1] == 2:
-                data[column_name] = transformed[:,0]
+                data[column_name] = transformed[:, 0]
                 new_column = '{}#{}'.format(column_name, 1)
-                data[new_column] = transformed[:,1]
+                data[new_column] = transformed[:, 1]
 
             else:
                 data[column_name] = transformed
@@ -96,11 +125,21 @@ class HyperTransformer:
         self.fit(data)
         return self.transform(data)
 
+    @staticmethod
+    def _get_columns(data, column_name):
+        regex = r'{}(#[0-9]+)?$'.format(column_name)
+        columns = data.columns[data.columns.str.match(regex)]
+        if len(columns) == 1:
+            return data.pop(columns[0])
+
+        return pd.concat([data.pop(column) for column in columns], axis=1)
+
     def reverse_transform(self, data):
         if self.copy:
             data = data.copy()
 
-        for column_name, transformer in self.transformers.items():
-            data[column_name] = transformer.reverse_transform(data[column_name])
+        for column_name, transformer in self._transformers.items():
+            columns = self._get_columns(data, column_name)
+            data[column_name] = transformer.reverse_transform(columns.values)
 
         return data
