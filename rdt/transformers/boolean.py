62dc5ed423dc5f5d94132c6bc82add45cd9c56f0
Carles Sala <carles@pythiac.com>
2019-10-07 10:29:05 +0200

    Add boolean transformer

---
+++ rdt/transformers/boolean.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+
+from rdt.transformers.base import BaseTransformer
+from rdt.transformers.null import NullTransformer
+
+
+class BooleanTransformer(BaseTransformer):
+    """Transformer for boolean data."""
+
+    null_transformer = None
+
+    def __init__(self, nan=-1, null_column=None):
+        self.nan = nan
+        self.null_column = null_column
+
+    def fit(self, data):
+        if isinstance(data, np.ndarray):
+            data = pd.Series(data)
+
+        if self.nan == 'ignore':
+            fill_value = None
+        else:
+            fill_value = self.nan
+
+        self.null_transformer = NullTransformer(fill_value, self.null_column)
+        self.null_transformer.fit(data)
+
+    def transform(self, data):
+        if isinstance(data, np.ndarray):
+            data = pd.Series(data)
+
+        data = data.astype(int)
+
+        return self.null_transformer.transform(data)
+
+    def reverse_transform(self, data):
+        if self.nan != 'ignore':
+            data = self.null_transformer.reverse_transform(data)
+
+        return np.round(data).astype(bool)
