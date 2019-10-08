31a45cc7327f551cda19cea81f3f2eec80fd235f
JDTheRipperPC <jdtheripperpc@gmail.com>
2019-10-08 15:04:57 +0200

    updated unittests: datetime, null, and numerical transformers

--- tests/transformers/test_datetime.py
+++ tests/transformers/test_datetime.py
@@ -1,118 +1,335 @@
 from unittest import TestCase
-from unittest.mock import Mock, call, patch
+from unittest.mock import Mock
 
 import numpy as np
 import pandas as pd
 
-from rdt.transformers import DateTimeTransformer
+from rdt.transformers import DatetimeTransformer
 
 
-class TestDTTransformer(TestCase):
+class TestDatetimeTransformer(TestCase):
 
     def test___init__(self):
+        """Test default instance"""
         # Run
-        transformer = DateTimeTransformer('%Y-%m-%d')
+        transformer = DatetimeTransformer()
 
         # Asserts
-        assert transformer.datetime_format == '%Y-%m-%d'
+        self.assertEqual(transformer.nan, 'mean', "Unexpected nan")
+        self.assertIsNone(transformer.null_column, "null_column is None by default")
+        self.assertIsNone(transformer.null_transformer, "null_transformer is None by default")
 
-    def test_transform_series(self):
-        """Transform a pandas.Series."""
+    def test__transform(self):
+        """Test transform datetimes series to integer"""
         # Setup
+        data = pd.Series([None, '1996-10-17', '1965-05-23'])
+        data = pd.to_datetime(data)
 
         # Run
-        transformer = Mock()
-        transformer.datetime_format = '%Y-%m-%d'
-
-        data = pd.Series(['1996-10-17', None, '1965-05-23'])
-
-        result = DateTimeTransformer.transform(transformer, data)
+        result = DatetimeTransformer._transform(data)
 
         # Asserts
-        expect = np.array([845510400000000000, -145497600000000000])
+        expect = pd.Series([np.nan, 845510400000000000, -145497600000000000])
 
-        assert np.array_equal(result, expect)
+        pd.testing.assert_series_equal(result, expect)
+
+    def test_fit_nan_mean_array(self):
+        """Test fit nan mean with numpy.array"""
+        # Setup
+        data = np.array([None, '1996-10-17', '1965-05-23'])
+        data = pd.to_datetime(data).to_numpy()
+
+        # Run
+        transformer = DatetimeTransformer(nan='mean')
+        transformer.fit(data)
+
+        # Asserts
+        expect_nan = 'mean'
+        expect_fill_value = 350006400000000000
+
+        self.assertEqual(
+            transformer.nan,
+            expect_nan,
+            'Unexpected nan'
+        )
+        self.assertEqual(
+            transformer.null_transformer.fill_value,
+            expect_fill_value,
+            "Data mean is wrong"
+        )
+
+    def test_fit_nan_mean_series(self):
+        """Test fit nan mean with pandas.Series"""
+        # Setup
+        data = pd.Series([None, '1996-10-17', '1965-05-23'])
+        data = pd.to_datetime(data)
+
+        # Run
+        transformer = DatetimeTransformer(nan='mean')
+        transformer.fit(data)
+
+        # Asserts
+        expect_nan = 'mean'
+        expect_fill_value = 350006400000000000
+
+        self.assertEqual(
+            transformer.nan,
+            expect_nan,
+            'Unexpected nan'
+        )
+        self.assertEqual(
+            transformer.null_transformer.fill_value,
+            expect_fill_value,
+            "Data mean is wrong"
+        )
+
+    def test_fit_nan_mode_array(self):
+        """Test fit nan mode with numpy.array"""
+        # Setup
+        data = np.array([None, '1996-10-17', '1965-05-23'])
+        data = pd.to_datetime(data).to_numpy()
+
+        # Run
+        transformer = DatetimeTransformer(nan='mode')
+        transformer.fit(data)
+
+        # Asserts
+        expect_nan = 'mode'
+        expect_fill_value = -145497600000000000
+
+        self.assertEqual(
+            transformer.nan,
+            expect_nan,
+            'Unexpected nan'
+        )
+        self.assertEqual(
+            transformer.null_transformer.fill_value,
+            expect_fill_value,
+            "Data mean is wrong"
+        )
+
+    def test_fit_nan_mode_series(self):
+        """Test fit nan mode with pandas.Series"""
+        # Setup
+        data = np.array([None, '1996-10-17', '1965-05-23'])
+        data = pd.to_datetime(data)
+
+        # Run
+        transformer = DatetimeTransformer(nan='mode')
+        transformer.fit(data)
+
+        # Asserts
+        expect_nan = 'mode'
+        expect_fill_value = -145497600000000000
+
+        self.assertEqual(
+            transformer.nan,
+            expect_nan,
+            'Unexpected nan'
+        )
+        self.assertEqual(
+            transformer.null_transformer.fill_value,
+            expect_fill_value,
+            "Data mean is wrong"
+        )
+
+    def test_fit_nan_ignore_array(self):
+        """Test fit nan ignore with numpy.array"""
+        # Setup
+        data = np.array([None, '1996-10-17', '1965-05-23'])
+        data = pd.to_datetime(data).to_numpy()
+
+        # Run
+        transformer = DatetimeTransformer(nan='ignore')
+        transformer.fit(data)
+
+        # Asserts
+        expect_nan = 'ignore'
+        expect_fill_value = None
+
+        self.assertEqual(
+            transformer.nan,
+            expect_nan,
+            'Unexpected nan'
+        )
+        self.assertEqual(
+            transformer.null_transformer.fill_value,
+            expect_fill_value,
+            "Data mean is wrong"
+        )
+
+    def test_fit_nan_ignore_series(self):
+        """Test fit nan ignore with pandas.Series"""
+        # Setup
+        data = np.array([None, '1996-10-17', '1965-05-23'])
+        data = pd.to_datetime(data)
+
+        # Run
+        transformer = DatetimeTransformer(nan='ignore')
+        transformer.fit(data)
+
+        # Asserts
+        expect_nan = 'ignore'
+        expect_fill_value = None
+
+        self.assertEqual(
+            transformer.nan,
+            expect_nan,
+            'Unexpected nan'
+        )
+        self.assertEqual(
+            transformer.null_transformer.fill_value,
+            expect_fill_value,
+            "Data mean is wrong"
+        )
+
+    def test_fit_nan_other_array(self):
+        """Test fit nan custom value with numpy.array"""
+        # Setup
+        data = np.array([None, '1996-10-17', '1965-05-23'])
+        data = pd.to_datetime(data).to_numpy()
+
+        # Run
+        transformer = DatetimeTransformer(nan=0)
+        transformer.fit(data)
+
+        # Asserts
+        expect_nan = 0
+        expect_fill_value = 0
+
+        self.assertEqual(
+            transformer.nan,
+            expect_nan,
+            'Unexpected nan'
+        )
+        self.assertEqual(
+            transformer.null_transformer.fill_value,
+            expect_fill_value,
+            "Data mean is wrong"
+        )
+
+    def test_fit_nan_other_series(self):
+        """Test fit nan custom value with pandas.Series"""
+        # Setup
+        data = np.array([None, '1996-10-17', '1965-05-23'])
+        data = pd.to_datetime(data)
+
+        # Run
+        transformer = DatetimeTransformer(nan=0)
+        transformer.fit(data)
+
+        # Asserts
+        expect_nan = 0
+        expect_fill_value = 0
+
+        self.assertEqual(
+            transformer.nan,
+            expect_nan,
+            'Unexpected nan'
+        )
+        self.assertEqual(
+            transformer.null_transformer.fill_value,
+            expect_fill_value,
+            "Data mean is wrong"
+        )
 
     def test_transform_array(self):
-        """Transform a numpy.array."""
+        """Test tranform datetime arary"""
         # Setup
+        data = np.array([None, '1996-10-17', '1965-05-23'])
+        data = pd.to_datetime(data).to_numpy()
+
+        data_transform = pd.Series([np.nan, 845510400000000000, -145497600000000000])
 
         # Run
         transformer = Mock()
-        transformer.datetime_format = '%Y-%m-%d'
+        transformer._transform.return_value = data_transform
 
-        data = np.array(['1996-10-17', None, '1965-05-23'])
-
-        result = DateTimeTransformer.transform(transformer, data)
+        DatetimeTransformer.transform(transformer, data)
 
         # Asserts
-        expect = np.array([845510400000000000, -145497600000000000])
+        exp_call_data = pd.Series([None, '1996-10-17', '1965-05-23'])
+        expect_call_args = pd.to_datetime(exp_call_data)
+        expect_call_count = 1
 
-        assert np.array_equal(result, expect)
+        pd.testing.assert_series_equal(
+            transformer._transform.call_args[0][0],
+            expect_call_args
+        )
+        self.assertEqual(
+            transformer.null_transformer.transform.call_count,
+            expect_call_count,
+            "NullTransformer.transform must be called only once."
+        )
 
-    def test__transform_to_date_epoch(self):
-        """Transform 0 into epoch time"""
+    def test_transform_series(self):
+        """Test transform datetime series"""
         # Setup
+        data = pd.Series([None, '1996-10-17', '1965-05-23'])
+        data = pd.to_datetime(data)
+
+        data_transform = pd.Series([np.nan, 845510400000000000, -145497600000000000])
 
         # Run
         transformer = Mock()
-        transformer.datetime_format = '%Y-%m-%d'
+        transformer._transform.return_value = data_transform
 
-        data = '0'
-
-        result = DateTimeTransformer._transform_to_date(transformer, data)
+        DatetimeTransformer.transform(transformer, data)
 
         # Asserts
-        expect = '1970-01-01'
+        exp_call_data = pd.Series([None, '1996-10-17', '1965-05-23'])
+        expect_call_args = pd.to_datetime(exp_call_data)
+        expect_call_count = 1
 
-        assert result == expect
+        pd.testing.assert_series_equal(
+            transformer._transform.call_args[0][0],
+            expect_call_args
+        )
+        self.assertEqual(
+            transformer.null_transformer.transform.call_count,
+            expect_call_count,
+            "NullTransformer.transform must be called only once."
+        )
 
-    def test__transform_to_date_pre_epoch(self):
-        """Transform a negative value into pre epoch time"""
+    def test_reverse_transform_nan_not_ignore(self):
+        """Test reverse_transform with nan not equal to ignore"""
         # Setup
+        data = pd.Series([np.nan, 845510400000000000, -145497600000000000])
+
+        reversed_data = pd.Series([np.nan, 845510400000000000, -145497600000000000])
 
         # Run
         transformer = Mock()
-        transformer.datetime_format = '%Y-%m-%d'
+        transformer.nan = 'mean'
+        transformer.null_transformer.reverse_transform.return_value = reversed_data
 
-        data = '-145497600000000000'
-
-        result = DateTimeTransformer._transform_to_date(transformer, data)
+        DatetimeTransformer.reverse_transform(transformer, data)
 
         # Asserts
-        expect = '1965-05-23'
+        expect_reverse_call_count = 1
 
-        assert result == expect
+        self.assertEqual(
+            transformer.null_transformer.reverse_transform.call_count,
+            expect_reverse_call_count,
+            "NullTransformer.reverse_transform must be called when nan is not ignore"
+        )
 
-    def test__transform_to_date_post_epoch(self):
-        """Transform positive value into post epoch time"""
+    def test_reverse_transform_nan_ignore(self):
+        """Test reverse_transform with nan equal to ignore"""
         # Setup
+        data = pd.Series([np.nan, 845510400000000000, -145497600000000000])
 
         # Run
         transformer = Mock()
-        transformer.datetime_format = '%Y-%m-%d'
+        transformer.nan = 'ignore'
 
-        data = '845510400000000000'
-
-        result = DateTimeTransformer._transform_to_date(transformer, data)
+        DatetimeTransformer.reverse_transform(transformer, data)
 
         # Asserts
-        expect = '1996-10-17'
+        expect_reverse_call_count = 0
 
-        assert result == expect
-
-    @patch('numpy.vectorize')
-    def test_reverse_transform(self, np_vectorize_mock):
-        # Setup
-
-        # Run
-        transformer = Mock()
-        transformer._transform_to_date = None
-
-        data = np.array([845510400000000000, -145497600000000000])
-
-        DateTimeTransformer.reverse_transform(transformer, data)
-
-        # Asserts
-        assert np_vectorize_mock.call_count == 1
-        assert np_vectorize_mock.call_args == call(None)
+        self.assertEqual(
+            transformer.null_transformer.reverse_transform.call_count,
+            expect_reverse_call_count,
+            "NullTransformer.reverse_transform won't be called when nan is ignore"
+        )
