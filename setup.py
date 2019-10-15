fc728ed808b21106826cc5a614876e2f7f144355
Carles Sala <carles@pythiac.com>
2019-10-15 10:36:56 -0400

    Bump version: 0.2.0 → 0.2.1.dev0

--- setup.py
+++ setup.py
@@ -85,6 +85,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/HDI-Project/RDT',
-    version='0.2.0',
+    version='0.2.1.dev0',
     zip_safe=False,
 )
