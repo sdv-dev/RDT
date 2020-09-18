64a196358c42b37e548000f3a01c25710f9e6e2e
Carles Sala <carles@pythiac.com>
2020-09-18 22:13:03 +0200

    Bump version: 0.2.5.dev1 → 0.2.5

--- setup.py
+++ setup.py
@@ -94,6 +94,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/RDT',
-    version='0.2.5.dev1',
+    version='0.2.5',
     zip_safe=False,
 )
