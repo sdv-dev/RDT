c740f7535172915b23b63fef80aaa30103df0185
Carles Sala <carles@pythiac.com>
2020-09-30 19:50:30 +0200

    Bump version: 0.2.6.dev0 → 0.2.6.dev1

--- rdt/__init__.py
+++ rdt/__init__.py
@@ -5,7 +5,7 @@
 
 __author__ = """MIT Data To AI Lab"""
 __email__ = 'dailabmit@gmail.com'
-__version__ = '0.2.6.dev0'
+__version__ = '0.2.6.dev1'
 
 import numpy as np
 import pandas as pd
--- setup.py
+++ setup.py
@@ -95,6 +95,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/RDT',
-    version='0.2.6.dev0',
+    version='0.2.6.dev1',
     zip_safe=False,
 )
