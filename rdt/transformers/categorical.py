3f673d17a3fd7ba3c68d0bb7fbc150d085637029
Felipe Alex Hofmann <fealho@gmail.com>
2020-11-11 18:12:32 -0600

    Raise error for non-existing category.

--- rdt/transformers/categorical.py
+++ rdt/transformers/categorical.py
@@ -246,7 +246,12 @@ class OneHotEncodingTransformer(BaseTransformer):
             numpy.ndarray:
         """
         dummies = pd.get_dummies(data, dummy_na=self.dummy_na)
-        return dummies.reindex(columns=self.dummies, fill_value=0).values.astype(int)
+        array = dummies.reindex(columns=self.dummies, fill_value=0).values.astype(int)
+        for i, col in enumerate(array.T):
+            if np.all(col == 0):
+                raise ValueError("The value '%s' was not seen during the fit stage." % data[i])
+        
+        return array
 
     def reverse_transform(self, data):
         """Convert float values back to the original categorical values.
