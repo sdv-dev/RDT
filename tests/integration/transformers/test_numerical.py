0f1f9980f4d7e7568e3e84b2e4ba444e0c798cf5
Carles Sala <carles@pythiac.com>
2020-11-19 20:47:23 +0100

    Adapt tests

--- tests/integration/transformers/test_numerical.py
+++ tests/integration/transformers/test_numerical.py
@@ -1,75 +1,127 @@
 import numpy as np
 
-from rdt.transformers.numerical import GaussianCopulaTransformer
+from rdt.transformers.numerical import GaussianCopulaTransformer, NumericalTransformer
 
 
-def test_copula_transformer_stats():
-    data = np.random.normal(loc=4, scale=4, size=1000)
+class TestNumericalTransformer:
 
-    ct = GaussianCopulaTransformer()
-    transformed = ct.fit_transform(data)
+    def test_null_column(self):
+        data = np.array([1, 2, 1, 2, np.nan, 1])
 
-    assert isinstance(transformed, np.ndarray)
-    assert transformed.shape == (1000, )
+        nt = NumericalTransformer()
+        transformed = nt.fit_transform(data)
 
-    np.testing.assert_almost_equal(transformed.mean(), 0, decimal=1)
-    np.testing.assert_almost_equal(transformed.std(), 1, decimal=1)
+        assert isinstance(transformed, np.ndarray)
+        assert transformed.shape == (6, 2)
+        assert list(transformed[:, 1]) == [0, 0, 0, 0, 1, 0]
 
-    reverse = ct.reverse_transform(transformed)
+        reverse = nt.reverse_transform(transformed)
 
-    np.testing.assert_array_almost_equal(reverse, data, decimal=2)
+        np.testing.assert_array_almost_equal(reverse, data, decimal=2)
 
+    def test_not_null_column(self):
+        data = np.array([1, 2, 1, 2, np.nan, 1])
 
-def test_copula_transformer_null_column():
-    data = np.array([1, 2, 1, 2, np.nan, 1])
+        nt = NumericalTransformer(null_column=False)
+        transformed = nt.fit_transform(data)
 
-    ct = GaussianCopulaTransformer()
-    transformed = ct.fit_transform(data)
+        assert isinstance(transformed, np.ndarray)
+        assert transformed.shape == (6, )
 
-    assert isinstance(transformed, np.ndarray)
-    assert transformed.shape == (6, 2)
-    assert list(transformed[:, 1]) == [0, 0, 0, 0, 1, 0]
+        reverse = nt.reverse_transform(transformed)
 
-    reverse = ct.reverse_transform(transformed)
+        np.testing.assert_array_almost_equal(reverse, data, decimal=2)
 
-    np.testing.assert_array_almost_equal(reverse, data, decimal=2)
+    def test_int(self):
+        data = np.array([1, 2, 1, 2, 1])
 
+        nt = NumericalTransformer(dtype=int)
+        transformed = nt.fit_transform(data)
 
-def test_copula_transformer_not_null_column():
-    data = np.array([1, 2, 1, 2, np.nan, 1])
+        assert isinstance(transformed, np.ndarray)
+        assert transformed.shape == (5, )
 
-    ct = GaussianCopulaTransformer(null_column=False)
-    transformed = ct.fit_transform(data)
+        reverse = nt.reverse_transform(transformed)
+        assert list(reverse) == [1, 2, 1, 2, 1]
 
-    assert isinstance(transformed, np.ndarray)
-    assert transformed.shape == (6, )
+    def test_int_nan(self):
+        data = np.array([1, 2, 1, 2, 1, np.nan])
 
-    reverse = ct.reverse_transform(transformed)
+        nt = NumericalTransformer(dtype=int)
+        transformed = nt.fit_transform(data)
 
-    np.testing.assert_array_almost_equal(reverse, data, decimal=2)
+        assert isinstance(transformed, np.ndarray)
+        assert transformed.shape == (6, 2)
 
-
-def test_copula_transformer_int():
-    data = np.array([1, 2, 1, 2, 1])
-
-    ct = GaussianCopulaTransformer(dtype=int)
-    transformed = ct.fit_transform(data)
-
-    assert isinstance(transformed, np.ndarray)
-    assert transformed.shape == (5, )
-
-    reverse = ct.reverse_transform(transformed)
-    assert list(reverse) == [1, 2, 1, 2, 1]
+        reverse = nt.reverse_transform(transformed)
+        np.testing.assert_array_almost_equal(reverse, data, decimal=2)
 
 
-def test_copula_transformer_int_nan():
-    data = np.array([1, 2, 1, 2, 1, np.nan])
+class TestGaussianCopulaTransformer:
 
-    ct = GaussianCopulaTransformer(dtype=int)
-    transformed = ct.fit_transform(data)
+    def test_stats(self):
+        data = np.random.normal(loc=4, scale=4, size=1000)
 
-    assert isinstance(transformed, np.ndarray)
-    assert transformed.shape == (6, 2)
+        ct = GaussianCopulaTransformer()
+        transformed = ct.fit_transform(data)
 
-    reverse = ct.reverse_transform(transformed)
-    np.testing.assert_array_almost_equal(reverse, data, decimal=2)
+        assert isinstance(transformed, np.ndarray)
+        assert transformed.shape == (1000, )
+
+        np.testing.assert_almost_equal(transformed.mean(), 0, decimal=1)
+        np.testing.assert_almost_equal(transformed.std(), 1, decimal=1)
+
+        reverse = ct.reverse_transform(transformed)
+
+        np.testing.assert_array_almost_equal(reverse, data, decimal=2)
+
+    def test_null_column(self):
+        data = np.array([1, 2, 1, 2, np.nan, 1])
+
+        ct = GaussianCopulaTransformer()
+        transformed = ct.fit_transform(data)
+
+        assert isinstance(transformed, np.ndarray)
+        assert transformed.shape == (6, 2)
+        assert list(transformed[:, 1]) == [0, 0, 0, 0, 1, 0]
+
+        reverse = ct.reverse_transform(transformed)
+
+        np.testing.assert_array_almost_equal(reverse, data, decimal=2)
+
+    def test_not_null_column(self):
+        data = np.array([1, 2, 1, 2, np.nan, 1])
+
+        ct = GaussianCopulaTransformer(null_column=False)
+        transformed = ct.fit_transform(data)
+
+        assert isinstance(transformed, np.ndarray)
+        assert transformed.shape == (6, )
+
+        reverse = ct.reverse_transform(transformed)
+
+        np.testing.assert_array_almost_equal(reverse, data, decimal=2)
+
+    def test_int(self):
+        data = np.array([1, 2, 1, 2, 1])
+
+        ct = GaussianCopulaTransformer(dtype=int)
+        transformed = ct.fit_transform(data)
+
+        assert isinstance(transformed, np.ndarray)
+        assert transformed.shape == (5, )
+
+        reverse = ct.reverse_transform(transformed)
+        assert list(reverse) == [1, 2, 1, 2, 1]
+
+    def test_int_nan(self):
+        data = np.array([1, 2, 1, 2, 1, np.nan])
+
+        ct = GaussianCopulaTransformer(dtype=int)
+        transformed = ct.fit_transform(data)
+
+        assert isinstance(transformed, np.ndarray)
+        assert transformed.shape == (6, 2)
+
+        reverse = ct.reverse_transform(transformed)
+        np.testing.assert_array_almost_equal(reverse, data, decimal=2)
