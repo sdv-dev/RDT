b0d6f4000a09d9764309fb4cee63f9314a829026
Neha Patki <nehapatki@gmail.com>
2021-07-12 10:50:18 -0400

    Bump version: 0.5.0 → 0.5.1.dev0

--- setup.py
+++ setup.py
@@ -100,6 +100,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/RDT',
-    version='0.5.0',
+    version='0.5.1.dev0',
     zip_safe=False,
 )
