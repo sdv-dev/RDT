8f3b91f3bf2fe8c411b471e783ba023f1cc93cd7
Andrew Montanez <amontanez2424@gmail.com>
2021-07-20 17:59:34 -0500

    lint and docstring

--- tests/performance/test_performance.py
+++ tests/performance/test_performance.py
@@ -25,15 +25,36 @@ def get_instance(obj, **kwargs):
 BASE = os.path.dirname(__file__)
 TESTS = glob.glob(BASE + '/test_cases/*.json')
 
+
 @pytest.mark.parametrize('config_path', TESTS)
 def test_performance(config_path):
+    """Run the performance tests for RDT.
+
+    This test should loop through every test config file,
+    load the transformer and dataset generator needed,
+    run the ``profile_transformer`` method against them
+    and assert that the memory consumption and times are under
+    the maximum acceptable values.
+
+    Input:
+    - Transformer loaded from config
+    - Dataset generator loaded from config
+    - fit size loaded from config
+    - transform size loaded from config
+
+    Output:
+    - pd.Series containing the memory and time for ``fit``,
+    ``transform`` and ``reverse_transform``. This should be
+    don for each specified test config file.
+    """
     with open(config_path, 'r') as config_file:
         config = json.load(config_file)
-    
+
     transformer = get_instance(config['transformer'], **config['kwargs'])
     dataset_gen = get_instance(config['dataset'])
 
-    out = profile_transformer(transformer, dataset_gen, config['transform_size'], config['fit_size'])
+    out = profile_transformer(transformer, dataset_gen, config['transform_size'],
+                              config['fit_size'])
 
     assert out['Fit Time'] < config['expected']['fit']['time']
     assert out['Fit Memory'] < config['expected']['fit']['memory']
