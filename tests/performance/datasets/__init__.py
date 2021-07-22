36dc948858105d7b07bb78fe67c8fd5deb914090
amontanez24 <amontanez2424@gmail.com>
2021-07-22 18:05:20 -0500

    add datetime generators

--- tests/performance/datasets/__init__.py
+++ tests/performance/datasets/__init__.py
@@ -1,9 +1,10 @@
 """Dataset Generators to test the RDT Transformers."""
 
-from tests.performance.datasets import numerical
+from tests.performance.datasets import datetime, numerical
 from tests.performance.datasets.base import BaseDatasetGenerator
 
 __all__ = [
+    'datetime',
     'numerical',
     'BaseDatasetGenerator',
 ]
