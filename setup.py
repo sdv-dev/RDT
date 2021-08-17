aa3d538e77c9eeeb82409a8db8a93300c29b65bd
Felipe Hofmann <fealho@gmail.com>
2021-08-16 20:41:33 -0700

    Bump version: 0.5.2.dev0 → 0.5.2.dev1

--- setup.py
+++ setup.py
@@ -101,6 +101,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/RDT',
-    version='0.5.2.dev0',
+    version='0.5.2.dev1',
     zip_safe=False,
 )
