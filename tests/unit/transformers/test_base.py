3e0040b25dfed86fb4c80cf5280b367aaac2f591
Felipe Hofmann <fealho@gmail.com>
2021-09-21 20:42:20 -0700

    Remove test cases.

--- tests/unit/transformers/test_base.py
+++ tests/unit/transformers/test_base.py
@@ -1,81 +1,29 @@
 from unittest import TestCase
-from unittest.mock import MagicMock
 
-import pandas as pd
-
-from rdt.transformers import BaseTransformer, CategoricalTransformer
+from rdt.transformers import BaseTransformer
 
 
-class TestBaseTransformer(TestCase):
+class TestNumericalTransformer(TestCase):
 
     def setUp(self):
-        self.data = pd.DataFrame({
-            'a': [1, 2, 3],
-            'b': ['x', 'y', 'z']
-        })
         self.base = BaseTransformer()
 
-    def test_get_input_type(self):
-        """Test `get_input_type` returns `_INPUT_TYPE` of child class."""
-        categorical_transformer = CategoricalTransformer()
-        categorical_transformer.INPUT_TYPE = MagicMock('categorical')
-        self.assertEqual(categorical_transformer.get_input_type(), 'categorical')
-
-    def test__add_prefix_none(self):
-        """Test `_add_prefix`."""
-        self.assertEqual(self.base._add_prefix(None, None), None)
-
-    def test__add_prefix(self):
-        """Test `_add_prefix`."""
-        column_to_type = {'digit': 'numerical', 'letter': 'categorical'}
-        expected = {'prefix.digit': 'numerical', 'prefix.letter': 'categorical'}
-        self.assertEqual(self.base._add_prefix(column_to_type, 'prefix'), expected)
-
-    def test_get_output_types(self):
-        """Test `get_output_types`."""
-        categorical_transformer = CategoricalTransformer()
-        categorical_transformer.OUTPUT_TYPES = MagicMock(
-            {'value': 'categorical', 'is_null': 'null'})
-        categorical_transformer._column_prefix = MagicMock('prefix')
-        expected = {'prefix.value': 'categorical', 'prefix.is_null': 'null'}
-        self.assertEqual(categorical_transformer.get_output_types(), expected)
-
-    def test_is_transform_deterministic(self):
-        """Test `is_transform_deterministic`."""
-        categorical_transformer = CategoricalTransformer()
-        categorical_transformer.DETERMINISTIC_TRANSFORM = MagicMock(False)
-        self.assertEqual(categorical_transformer.is_transform_deterministic(), False)
-
-    def test__fit(self):
+    def test_fit(self):
         """Test call fit and pass"""
         with self.assertRaises(NotImplementedError):
-            self.base._fit(None)
+            self.base.fit(None)
 
-    def test__transform(self):
+    def test_transform(self):
         """Test call transform raise not implemented error"""
         with self.assertRaises(NotImplementedError):
-            self.base._transform(None)
+            self.base.transform(None)
 
     def test_fit_transform(self):
         """Test call fit_transform raise not implemented error"""
         with self.assertRaises(NotImplementedError):
-            self.base.fit_transform(self.data, ['a'])
+            self.base.fit_transform(None)
 
-    def test__reverse_transform(self):
+    def test_reverse_transform(self):
         """Test call reverse_transform raise not implemented error"""
         with self.assertRaises(NotImplementedError):
-            self.base._reverse_transform(None)
-
-    def test_fit1(self):
-        """Test the fit method."""
-        with self.assertRaises(NotImplementedError):
-            self.base.fit(self.data, ['a'])
-
-        assert self.base._column_prefix == 'a'
-
-    def test_fit2(self):
-        """Test the fit method."""
-        with self.assertRaises(NotImplementedError):
-            self.base.fit(self.data, ['a', 'b'])
-
-        assert self.base._column_prefix == 'a#b'
+            self.base.reverse_transform(None)
