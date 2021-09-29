860591a5aae188710fae44d6508556670ffc833c
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-09-29 21:11:44 +0200

    Fix lint

--- tests/unit/transformers/addons/identity/test_indentity.py
+++ tests/unit/transformers/addons/identity/test_indentity.py
@@ -2,7 +2,6 @@ from unittest import TestCase
 from unittest.mock import Mock
 
 import numpy as np
-import pandas as pd
 
 from rdt.transformers import IdentityTransformer
 
@@ -44,7 +43,6 @@ class TestIdentityTransformer(TestCase):
 
         assert instance.INPUT_TYPE == expected_input_type
         assert instance.OUTPUT_TYPES == expected_output_types
-
 
     def test__transform(self):
         """Test ``IdentityTransformer._transform`` function.
