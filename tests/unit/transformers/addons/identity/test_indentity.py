3aa41614b1ba1f24bc9f23a823284e49e4dd3ea6
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-09-29 13:49:00 +0200

    Unit tests

--- tests/unit/transformers/addons/identity/test_indentity.py
+++ tests/unit/transformers/addons/identity/test_indentity.py
@@ -1,20 +1,58 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
 import numpy as np
 import pandas as pd
 
 from rdt.transformers import IdentityTransformer
 
 
-class TestIdentityTransformer:
+class TestIdentityTransformer(TestCase):
+
+    def test___init__(self):
+        instance = IdentityTransformer()
+        assert instance.INPUT_TYPE is None
+        assert instance.OUTPUT_TYPES is None
 
     def test_fit(self):
+        # setup
         data = pd.DataFrame({
             'a': np.array([1, 2, 3]),
             'b': np.array(['a', 'b', 'c']),
             'c': np.array([1., 2., 3.])
         })
 
+        _fit_mock = Mock()
         instance = IdentityTransformer()
+        instance._fit = _fit_mock
+
+        instance.INPUT_TYPE = {'a': 'int', 'b': 'object'}
+        instance.OUTPUT_TYPES = {'a': 'int', 'b': 'object'}
+
+        # run
         instance.fit(data, columns=['a', 'b'])
+
+        # assert
+        _fit_data_arg = _fit_mock.call_args[0][0]
+        pd.testing.assert_frame_equal(data[['a', 'b']], _fit_data_arg)
+
+    def test__fit(self):
+        # setup
+        data = pd.DataFrame({
+            'a': np.array([1, 2, 3]),
+            'b': np.array(['a', 'b', 'c']),
+            'c': np.array([1., 2., 3.])
+        })
+        instance = IdentityTransformer()
+
+        # run
+        instance.fit(data, columns=['a', 'b'])
+
+        # assert
+        dtypes = data.dtypes[['a', 'b']]
+
+        instance.INPUT_TYPE == dict(dtypes)
+        instance.OUTPUT_TYPES == dict(dtypes)
 
     def test_transform(self):
         data = pd.DataFrame({
@@ -24,7 +62,6 @@ class TestIdentityTransformer:
         })
 
         instance = IdentityTransformer()
-        instance.fit(data, columns=['a', 'b'])
         transformed = instance.transform(data)
 
         # assert
@@ -38,7 +75,6 @@ class TestIdentityTransformer:
         })
 
         instance = IdentityTransformer()
-        instance.fit(data, columns=['a', 'b'])
         reverse_transformed = instance.reverse_transform(data)
 
         # assert
