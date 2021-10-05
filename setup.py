543cdd60c36130ec60029b180b7d4af9151bc9d3
Plamen Valentinov Kolev <41479552+pvk-developer@users.noreply.github.com>
2021-10-05 18:15:34 +0200

    Add pep8-naming addon for flake8 (#275)

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ development_requires = [
     'flake8-print>=4.0.0,<4.1',
     'isort>=4.3.4,<5',
     'pylint>=2.5.3,<3',
+    'pep8-naming>=0.12.1,<0.13',
 
     # fix style issues
     'autoflake>=1.1,<2',
