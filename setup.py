085394f2cb6a7dfd56126a8b44d7c7d6dc450b33
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-10-05 11:43:10 +0200

    Add pandas vet dependency

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ development_requires = [
     'flake8-sfs>=0.0.3,<0.1',
     'isort>=4.3.4,<5',
     'pylint>=2.5.3,<3',
+    'pandas-vet>=0.2.2,<0.3',
 
     # fix style issues
     'autoflake>=1.1,<2',
