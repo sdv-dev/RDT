7b1f3dd2f46e791490061fff7b81605e497360e5
Katharine Xiao <2405771+katxiao@users.noreply.github.com>
2021-10-07 16:48:18 -0400

    Bump version: 0.5.3.dev0 → 0.5.3.dev1

--- setup.py
+++ setup.py
@@ -101,6 +101,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/RDT',
-    version='0.5.3.dev0',
+    version='0.5.3.dev1',
     zip_safe=False,
 )
