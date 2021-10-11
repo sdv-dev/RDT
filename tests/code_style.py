3416154abdc5e49e3c79861be949ce6c4a195aed
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2021-10-11 12:08:12 +0200

    Add test name validations

--- tests/code_style.py
+++ tests/code_style.py
@@ -5,6 +5,7 @@ import inspect
 import json
 import re
 from pathlib import Path
+from types import FunctionType
 
 from rdt.transformers.base import BaseTransformer
 
@@ -88,13 +89,10 @@ def validate_transformer_addon(transformer):
         return all([init_file_exist, config_json_exist, config_json_is_valid, module_py])
 
 
-def validate_test_location(transformer):
-    """Validate if the test file exists in the expected location."""
-    valid_test_location = False
-
+def _get_test_location(transformer):
     transformer_file = Path(inspect.getfile(transformer))
     transformer_folder = transformer_file.parent
-    rdt_unit_test_path = Path(__file__) / 'unit'
+    rdt_unit_test_path = Path(__file__).parent / 'unit'
     test_location = None
 
     if transformer_folder.match('transformers'):
@@ -108,7 +106,64 @@ def validate_test_location(transformer):
         test_location = rdt_unit_test_path / 'transformers' / 'addons'
         test_location = test_location / f'test_{transformer_file.name}'
 
+    return test_location
+
+
+def validate_test_location(transformer):
+    """Validate if the test file exists in the expected location."""
+    test_location = _get_test_location(transformer)
     if test_location is None:
         return False
 
     return test_location.exists()
+
+
+def validate_test_names(transformer):
+    """Validate if the test methods are properly specified."""
+    test_file = _get_test_location(transformer)
+
+    # load the module
+    spec = importlib.util.spec_from_file_location(f'Test{transformer.__name__}', test_file)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    # get test class
+    try:
+        test_class = getattr(module, f'Test{transformer.__name__}')
+
+    except AttributeError:
+        return False  # The test class doesn't exist
+
+    # get the test methods that start with test
+    test_functions = inspect.getmembers(test_class, predicate=inspect.isfunction)
+    test_functions = [
+        test
+        for test, _ in test_functions
+        if test.startswith('test')
+    ]
+
+    if not test_functions:
+        return False  # No test functions found
+
+    transformer_functions = [
+        name
+        for name, function in transformer.__dict__.items()
+        if isinstance(function, (FunctionType, classmethod, staticmethod))
+    ]
+
+    test_function_exists = []
+
+    for transformer_function in transformer_functions:
+        count = len(test_function_exists)
+        simple_test = f'test_{transformer_function}'
+        described_test = f'test_{transformer_function}_'
+        for test in test_functions:
+            if test.startswith(described_test):
+                test_function_exists.append(True)
+            if test.startswith(simple_test):
+                test_function_exists.append(True)
+
+        if count == len(test_function_exists):
+            test_function_exists.append(False)
+
+    return len(test_function_exists) >= len(transformer_functions)
