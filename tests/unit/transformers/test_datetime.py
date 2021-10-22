b44e41e4cbb017eb6c1788cf1e30dc6e2a0edb6b
Carles Sala <carles@pythiac.com>
2021-10-22 22:18:28 +0200

    100% null transformer coverage

--- tests/unit/transformers/test_datetime.py
+++ tests/unit/transformers/test_datetime.py
@@ -13,19 +13,8 @@ class TestDatetimeTransformer:
 
         output = dtt._reverse_transform(np.array([None]))
 
-        expected = pd.to_datetime(['NaT'])
-        pd.testing.assert_series_equal(output.to_series(), expected.to_series())
-
-    def test__reverse_transform_2d_ndarray(self):
-        dt = pd.to_datetime(['2020-01-01', '2020-02-01', '2020-03-01'])
-        dtt = DatetimeTransformer(strip_constant=True)
-        dtt._fit(dt)
-
-        transformed = np.array([[18262.], [18293.], [18322.]])
-        output = dtt._reverse_transform(transformed)
-
-        expected = pd.to_datetime(['2020-01-01', '2020-02-01', '2020-03-01'])
-        pd.testing.assert_series_equal(output.to_series(), expected.to_series())
+        expected = pd.Series(pd.to_datetime(['NaT']))
+        pd.testing.assert_series_equal(output, expected)
 
 
 class TestDatetimeRoundedTransformer:
