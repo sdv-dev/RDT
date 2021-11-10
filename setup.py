84d4dc24766a94b11306c805f1ac21c0438cd1fc
amontanez24 <amontanez2424@gmail.com>
2021-11-09 19:39:18 -0600

    Bump version: 0.6.1.dev0 → 0.6.1.dev1

--- setup.py
+++ setup.py
@@ -130,6 +130,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/RDT',
-    version='0.6.1.dev0',
+    version='0.6.1.dev1',
     zip_safe=False,
 )
