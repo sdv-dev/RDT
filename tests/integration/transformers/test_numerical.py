38b0c6090a21500256c7defbd4e47bd9c67f1b7d
Felipe Alex Hofmann <fealho@gmail.com>
2021-12-17 09:57:16 -0800

    Add a bayesian gaussian mixture model transformer (#329)
    * Add bgmm transformer
    
    * Add test cases
    
    * Adds integration tests
    
    * .
    
    * Working version
    
    * Remove bayesian_gmm file
    
    * Update code
    
    * Redesing the code
    
    * Remove correct file
    
    * Correct constants
    
    * General code improvements
    
    * Add scikit-learn
    
    * Loads of improvements
    
    * Add docstrings + fixes
    
    * Add null support
    
    * Not identity reverse
    
    * Add null test
    
    * Add new tests
    
    * 100% cov
    
    * Remove randomness
    
    * Lots of improvements
    
    * .
    
    * Add docstring
    
    * Address feedback
    
    * Remove datasets
    
    * Remove unnecessary files
    
    * Fix bug
    
    * Remove randomness of test
    
    * Fix lint
    
    * Increase data size
    
    * Ensure tests always pass
    
    * CHange variable name
    
    * Correct place

--- tests/integration/transformers/test_numerical.py
+++ tests/integration/transformers/test_numerical.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 
-from rdt.transformers.numerical import GaussianCopulaTransformer, NumericalTransformer
+from rdt.transformers.numerical import (
+    BayesGMMTransformer, GaussianCopulaTransformer, NumericalTransformer)
 
 
 class TestNumericalTransformer:
@@ -135,3 +136,85 @@ class TestGaussianCopulaTransformer:
 
         reverse = ct.reverse_transform(transformed)
         np.testing.assert_array_almost_equal(reverse, data, decimal=2)
+
+
+class TestBayesGMMTransformer:
+
+    def generate_data(self):
+        data1 = np.random.normal(loc=5, scale=1, size=100)
+        data2 = np.random.normal(loc=-5, scale=1, size=100)
+        data = np.concatenate([data1, data2])
+
+        return pd.DataFrame(data, columns=['col'])
+
+    def test_dataframe(self):
+        data = self.generate_data()
+
+        bgmm_transformer = BayesGMMTransformer()
+        bgmm_transformer.fit(data, list(data.columns))
+        transformed = bgmm_transformer.transform(data)
+
+        assert isinstance(transformed, pd.DataFrame)
+        assert transformed.shape == (200, 2)
+        assert all(isinstance(x, float) for x in transformed['col.normalized'])
+        assert all(isinstance(x, float) for x in transformed['col.component'])
+
+        reverse = bgmm_transformer.reverse_transform(transformed)
+        np.testing.assert_array_almost_equal(reverse, data, decimal=1)
+
+    def test_nulls(self):
+        data = self.generate_data()
+        mask = np.random.choice([1, 0], data.shape, p=[.1, .9]).astype(bool)
+        data[mask] = np.nan
+
+        bgmm_transformer = BayesGMMTransformer()
+        bgmm_transformer.fit(data, list(data.columns))
+        transformed = bgmm_transformer.transform(data)
+
+        assert isinstance(transformed, pd.DataFrame)
+        assert transformed.shape == (200, 3)
+        assert all(isinstance(x, float) for x in transformed['col.normalized'])
+        assert all(isinstance(x, float) for x in transformed['col.component'])
+        assert all(isinstance(x, float) for x in transformed['col.is_null'])
+
+        reverse = bgmm_transformer.reverse_transform(transformed)
+        np.testing.assert_array_almost_equal(reverse, data, decimal=1)
+
+    def test_data_different_sizes(self):
+        data = np.concatenate([
+            np.random.normal(loc=5, scale=1, size=100),
+            np.random.normal(loc=100, scale=1, size=500),
+        ])
+        data = pd.DataFrame(data, columns=['col'])
+
+        bgmm_transformer = BayesGMMTransformer()
+        bgmm_transformer.fit(data, list(data.columns))
+        transformed = bgmm_transformer.transform(data)
+
+        assert isinstance(transformed, pd.DataFrame)
+        assert all(isinstance(x, float) for x in transformed['col.normalized'])
+        assert all(isinstance(x, float) for x in transformed['col.component'])
+
+        reverse = bgmm_transformer.reverse_transform(transformed)
+        np.testing.assert_array_almost_equal(reverse, data, decimal=1)
+
+    def test_multiple_components(self):
+        data = np.concatenate([
+            np.random.normal(loc=5, scale=0.02, size=300),
+            np.random.normal(loc=-4, scale=0.1, size=1000),
+            np.random.normal(loc=-180, scale=3, size=1500),
+            np.random.normal(loc=100, scale=10, size=500),
+        ])
+        data = pd.DataFrame(data, columns=['col'])
+        data = data.sample(frac=1).reset_index(drop=True)
+
+        bgmm_transformer = BayesGMMTransformer()
+        bgmm_transformer.fit(data, list(data.columns))
+        transformed = bgmm_transformer.transform(data)
+
+        assert isinstance(transformed, pd.DataFrame)
+        assert all(isinstance(x, float) for x in transformed['col.normalized'])
+        assert all(isinstance(x, float) for x in transformed['col.component'])
+
+        reverse = bgmm_transformer.reverse_transform(transformed)
+        np.testing.assert_array_almost_equal(reverse, data, decimal=1)
