8f39a80d82652b746b5500cec37b8fa1752109d3
Felipe Hofmann <fealho@gmail.com>
2022-02-02 09:10:35 -0800

    Add test case for get_demo

--- tests/unit/test___init__.py
+++ tests/unit/test___init__.py
@@ -8,3 +8,10 @@ def test_get_demo():
         'last_login', 'email_optin', 'credit_card', 'age', 'dollars_spent']
     assert len(demo) == 5
     assert list(demo.isna().sum(axis=0)) == [1, 1, 1, 0, 1]
+
+def test_get_demo_many_rows():
+    demo = get_demo(100)
+
+    assert len(demo) == 100
+    assert list(demo.columns) == [
+        'last_login', 'email_optin', 'credit_card', 'age', 'dollars_spent']
