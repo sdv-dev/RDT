d37afabded783495b47b32df6323e4d56d5cd24f
Felipe Hofmann <fealho@gmail.com>
2022-05-12 15:16:30 -0700

    Remove logic to round integers

--- rdt/transformers/numerical.py
+++ rdt/transformers/numerical.py
@@ -102,13 +102,6 @@ class FloatFormatter(BaseTransformer):
                 if (roundable_data == roundable_data.round(decimal)).all():
                     return decimal
 
-        elif len(roundable_data) > 0:
-            maximum = max(abs(roundable_data))
-            start = int(np.log10(maximum)) if maximum != 0 else 0
-            for decimal in range(-start, 1):
-                if (roundable_data == roundable_data.round(decimal)).all():
-                    return decimal
-
         return None
 
     def _fit(self, data):
