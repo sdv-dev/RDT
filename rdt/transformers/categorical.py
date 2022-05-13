fixing nans and removing code to convert strings to numerical

rdt/transformers/categorical.py
@@ -452,28 +452,24 @@ class LabelEncoder(BaseTransformer):
             if unique_data.dtype.type not in [np.str_, np.object_]:
                 raise Error("The data must be of type string if order_by is 'alphabetical'.")
 
-            unique_data = np.sort(unique_data)
+            nans = pd.isna(unique_data)
+            if nans.any():
+                nan_rep = unique_data[nans][0]
+                unique_data = np.sort(unique_data[~nans])
+                unique_data = np.append(unique_data, [nan_rep])
+
+            else:
+                unique_data = np.sort(unique_data)
 
         elif self.order_by == 'numerical_value':
-            error_message = (
-                'The data must be numerical or able to be casted as a float if order_by '
-                "is 'numerical_value'."
-            )
-            if unique_data.dtype.type in [np.str_, np.object_]:
-                try:
-                    unique_data.astype(np.float)
-                    unique_data = list(unique_data)
-                    unique_data.sort(key=float)
-                    unique_data = np.array(unique_data)
-
-                except ValueError as error:
-                    raise Error(error_message) from error
-
-            elif np.issubdtype(unique_data.dtype.type, np.number):
+            if np.issubdtype(unique_data.dtype.type, np.number):
                 unique_data = np.sort(unique_data)
 
             else:
-                raise Error(error_message)
+                raise Error(
+                    'The data must be numerical or able to be casted as a float if order_by '
+                    "is 'numerical_value'."
+                )
 
         return unique_data
 

tests/integration/transformers/test_categorical.py
@@ -316,37 +316,16 @@ def test_label_encoder_order_by_numerical():
         - Transformed data should map labels to values based on numerical order.
     """
 
-    data = pd.DataFrame([5, 3.11, 100, 67.8, -2.5], columns=['column_name'])
+    data = pd.DataFrame([5, np.nan, 3.11, 100, 67.8, -2.5], columns=['column_name'])
 
     transformer = LabelEncoder(order_by='numerical_value')
     transformer.fit(data, 'column_name')
     transformed = transformer.transform(data)
+    reverse = transformer.reverse_transform(transformed)
 
-    expected = pd.DataFrame([2, 1, 4, 3, 0], columns=['column_name.value'])
+    expected = pd.DataFrame([2, 5, 1, 4, 3, 0], columns=['column_name.value'])
     pd.testing.assert_frame_equal(transformed, expected)
-
-
-def test_label_encoder_order_by_numerical_as_string():
-    """Test the LabelEncoder appropriately transforms data if `order_by` is 'numerical_value'.
-
-    If the data is text, but can be converted to numbers, the labels should still be applied
-    in numerical order.
-
-    Input:
-        - pandas.DataFrame of string data that can be numeric.
-
-    Output:
-        - Transformed data should map labels to values based on numerical order.
-    """
-
-    data = pd.DataFrame(['5', '3.11', '100', '67.8', '-2.5'], columns=['column_name'])
-
-    transformer = LabelEncoder(order_by='numerical_value')
-    transformer.fit(data, 'column_name')
-    transformed = transformer.transform(data)
-
-    expected = pd.DataFrame([2, 1, 4, 3, 0], columns=['column_name.value'])
-    pd.testing.assert_frame_equal(transformed, expected)
+    pd.testing.assert_frame_equal(reverse, data)
 
 
 def test_label_encoder_order_by_alphabetical():
@@ -359,11 +338,13 @@ def test_label_encoder_order_by_alphabetical():
         - Transformed data should map labels to values based on alphabetical order.
     """
 
-    data = pd.DataFrame(['one', 'two', 'three', 'four'], columns=['column_name'])
+    data = pd.DataFrame(['one', 'two', np.nan, 'three', 'four'], columns=['column_name'])
 
     transformer = LabelEncoder(order_by='alphabetical')
     transformer.fit(data, 'column_name')
     transformed = transformer.transform(data)
+    reverse = transformer.reverse_transform(transformed)
 
-    expected = pd.DataFrame([1, 3, 2, 0], columns=['column_name.value'])
+    expected = pd.DataFrame([1, 3, 4, 2, 0], columns=['column_name.value'])
     pd.testing.assert_frame_equal(transformed, expected)
+    pd.testing.assert_frame_equal(reverse, data)

tests/unit/transformers/test_categorical.py
@@ -1565,6 +1565,29 @@ class TestLabelEncoder:
         # Assert
         np.testing.assert_array_equal(ordered, np.array(['four', 'one', 'three', 'two']))
 
+    def test__order_categories_alphabetical_with_nans(self):
+        """Test the ``_order_categories`` method when ``order_by`` is 'alphabetical'.
+
+        Setup:
+            - Set ``order_by`` to 'alphabetical'.
+
+        Input:
+            - numpy array of strings that are unordered and have nans.
+
+        Output:
+            - Same numpy array but with the strings alphabetically ordered and nan at the end.
+        """
+        # Setup
+        transformer = LabelEncoder(order_by='alphabetical')
+        arr = np.array(['one', 'two', 'three', np.nan, 'four'], dtype='object')
+
+        # Run
+        ordered = transformer._order_categories(arr)
+
+        # Assert
+        expected = np.array(['four', 'one', 'three', 'two', np.nan], dtype='object')
+        pd.testing.assert_series_equal(pd.Series(ordered), pd.Series(expected))
+
     def test__order_categories_alphabetical_error(self):
         """Test the ``_order_categories`` method when ``order_by`` is 'alphabetical'.
 
@@ -1603,38 +1626,13 @@ class TestLabelEncoder:
         """
         # Setup
         transformer = LabelEncoder(order_by='numerical_value')
-        arr = np.array([5, 3.11, 100, 67.8, -2.5])
+        arr = np.array([5, 3.11, 100, 67.8, np.nan, -2.5])
 
         # Run
         ordered = transformer._order_categories(arr)
 
         # Assert
-        np.testing.assert_array_equal(ordered, np.array([-2.5, 3.11, 5, 67.8, 100]))
-
-    def test__order_categories_numerical_represented_as_strings(self):
-        """Test the ``_order_categories`` method when ``order_by`` is 'numerical_value'.
-
-        If the array is made up of strings that can be converted to floats, and `order_by`
-        is 'numerical_value', then we should still order the categories.
-
-        Setup:
-            - Set ``order_by`` to 'numerical_value'.
-
-        Input:
-            - numpy array of strings that can be floats and are unordered.
-
-        Output:
-            - Same numpy array but with the strings ordered numerically.
-        """
-        # Setup
-        transformer = LabelEncoder(order_by='numerical_value')
-        arr = np.array(['5', '3.11', '100', '67.8', '-2.5'])
-
-        # Run
-        ordered = transformer._order_categories(arr)
-
-        # Assert
-        np.testing.assert_array_equal(ordered, np.array(['-2.5', '3.11', '5', '67.8', '100']))
+        np.testing.assert_array_equal(ordered, np.array([-2.5, 3.11, 5, 67.8, 100, np.nan]))
 
     def test__order_categories_numerical_error(self):
         """Test the ``_order_categories`` method when ``order_by`` is 'numerical_value'.