494d9f0444b7770553b7d4edd8d2a4ff19dab8cc
Plamen Valentinov Kolev <pvkdeveloper@gmx.com>
2022-09-10 10:52:09 +0200

    Remove NullTransformer from RegexGenerator and AnonymizedFaker

--- tests/unit/transformers/pii/test_anonymizer.py
+++ tests/unit/transformers/pii/test_anonymizer.py
@@ -10,7 +10,6 @@ import pandas as pd
 import pytest
 
 from rdt.errors import Error
-from rdt.transformers.null import NullTransformer
 from rdt.transformers.pii.anonymizer import AnonymizedFaker, PseudoAnonymizedFaker
 
 
@@ -200,8 +199,6 @@ class TestAnonymizedFaker:
             - the ``instance.function_name`` is ``lexify``.
             - the ``instance.locales`` is ``None``.
             - the ``instance.function_kwargs`` is an empty ``dict``.
-            - the ``instance.missing_value_replacement`` is ``None``.
-            - the ``instance.model_missing_values`` is ``False``
             - ``check_provider_function`` has been called once with ``BaseProvider`` and
               ``lexify``.
             - the ``instance._function`` is ``instance.faker.lexify``.
@@ -214,8 +211,6 @@ class TestAnonymizedFaker:
         assert instance.provider_name == 'BaseProvider'
         assert instance.function_name == 'lexify'
         assert instance.function_kwargs == {}
-        assert instance.missing_value_replacement is None
-        assert not instance.model_missing_values
         assert instance.locales is None
         assert mock_faker.Faker.called_once_with(None)
         assert instance.enforce_uniqueness is False
@@ -240,8 +235,6 @@ class TestAnonymizedFaker:
             - the ``instance.function_name`` is ``credit_card_full``.
             - the ``instance.locales`` is ``['en_US', 'fr_FR']``.
             - the ``instance.function_kwargs`` is ``{'type': 'visa'}``.
-            - the ``instance.missing_value_replacement`` is ``None``.
-            - the ``instance.model_missing_values`` is ``False``
             - ``check_provider_function`` has been called once with ``CreditCard`` and
               ``credit_card_full``.
             - the ``instance._function`` is ``instance.faker.credit_card_full``.
@@ -262,8 +255,6 @@ class TestAnonymizedFaker:
         assert instance.provider_name == 'credit_card'
         assert instance.function_name == 'credit_card_full'
         assert instance.function_kwargs == {'type': 'visa'}
-        assert instance.missing_value_replacement is None
-        assert not instance.model_missing_values
         assert instance.locales == ['en_US', 'fr_FR']
         assert mock_faker.Faker.called_once_with(['en_US', 'fr_FR'])
         assert instance.enforce_uniqueness
@@ -284,92 +275,34 @@ class TestAnonymizedFaker:
         with pytest.raises(Error, match=expected_message):
             AnonymizedFaker(provider_name='credit_card', locales=['en_US', 'fr_FR'])
 
-    def test_get_output_sdtypes(self):
-        """Test the ``get_output_sdtypes``.
-
-        Setup:
-            - initialize a ``AnonymizedFaker`` transformer with default values.
-
-        Output:
-            - the ``output_sdtypes`` returns an empty dictionary.
-        """
-        # Setup
-        transformer = AnonymizedFaker()
-        transformer.column_prefix = 'a#b'
-
-        # Run
-        output = transformer.get_output_sdtypes()
-
-        # Assert
-        expected = {}
-        assert output == expected
-
-    def test_get_output_sdtypes_model_missing_values(self):
-        """Test the ``get_output_sdtypes`` method when a null column is created.
-
-        Setup:
-            - initialize a ``AnonymizedFaker`` transformer which:
-                - sets ``self.null_transformer`` to a ``NullTransformer`` where
-                ``self.model_missing_values`` is True.
-                - sets ``self.column_prefix`` to a string.
-
-        Output:
-            - An ``output_sdtypes`` dictionary is being returned with the ``self.column_prefix``
-              added to the beginning of the keys.
-        """
-        # Setup
-        transformer = AnonymizedFaker()
-        transformer.null_transformer = NullTransformer(missing_value_replacement='fill')
-        transformer.null_transformer._model_missing_values = True
-        transformer.column_prefix = 'a#b'
-
-        # Run
-        output = transformer.get_output_sdtypes()
-
-        # Assert
-        expected = {
-            'a#b.is_null': 'float'
-        }
-        assert output == expected
-
-    @patch('rdt.transformers.pii.anonymizer.NullTransformer')
-    def test__fit(self, mock_null_transformer):
+    def test__fit(self):
         """Test the ``_fit`` method.
 
-        Validate that the ``_fit`` method uses the ``NullTransformer`` to parse the data
-        and learn the length of it.
+        Validate that the ``_fit`` method learns the size of the input data.
 
         Setup:
             - Initialize a ``AnonymizedFaker`` transformer.
-            - Mock the ``NullTransformer``.
 
         Input:
             - ``pd.Series`` containing 3 strings.
 
         Side Effects:
-            - ``NullTransformer`` instance has been created with ``model_missing_values`` as
-              ``False`` and ``missing_value_replacement`` as ``None``.
-            - ``ǸullTransformer`` instance method ``fit`` has been called with the input data.
             - ``instance.data_length`` equals to the length of the input data.
         """
         # Setup
         transformer = AnonymizedFaker()
-
         columns_data = pd.Series(['1', '2', '3'])
 
         # Run
         transformer._fit(columns_data)
 
         # Assert
-        mock_null_transformer.assert_called_once_with(None, False)
-        mock_null_transformer.return_value.fit.called_once_with(columns_data)
         assert transformer.data_length == 3
 
     def test__transform(self):
         """Test the ``_transform`` method.
 
-        Validate that the ``_transform`` method returns ``None`` when the ``NullTransformer``
-        does not model the missing values.
+        Validate that the ``_transform`` method returns ``None``.
 
         Setup:
             - Initialize a ``AnonymizedFaker`` transformer.
@@ -390,42 +323,6 @@ class TestAnonymizedFaker:
         # Assert
         assert result is None
 
-    def test__transform_model_missing_values(self):
-        """Test the ``_transform`` method.
-
-        Validate that the ``_transform`` method uses the ``NullTransformer`` instance to
-        transform the data.
-
-        Setup:
-            - Initialize a ``AnonymizedFaker`` transformer.
-            - Mock the ``null_transformer`` of the instance.
-            - Mock the return value of the ``null_transformer.transform``.
-
-        Input:
-            - ``pd.Series`` with three values.
-
-        Output:
-            - The second dimension of the mocked return value of the
-              ``null_transformer.transform``.
-        """
-        # Setup
-        columns_data = pd.Series([1, 2, 3])
-        instance = AnonymizedFaker()
-        instance.null_transformer = Mock()
-
-        instance.null_transformer.transform.return_value = np.array([
-            [4, 0],
-            [5, 1],
-            [6, 0],
-        ])
-
-        # Run
-        result = instance._transform(columns_data)
-
-        # Assert
-        instance.null_transformer.transform.assert_called_once_with(columns_data)
-        np.testing.assert_array_equal(result, np.array([0, 1, 0]))
-
     def test__reverse_transform(self):
         """Test the ``_reverse_transform`` method.
 
@@ -434,96 +331,28 @@ class TestAnonymizedFaker:
 
         Setup:
             - Initialize a ``AnonymizedFaker`` transformer.
-            - Mock the ``null_transformer`` of the instance.
-            - Mock the return value of the ``null_transformer.reverse_transform``.
 
         Input:
             - ``pd.Series`` with three values.
 
         Output:
-            - the output of ``null_transformer.reverse_transform``.
+            - the output a ``numpy.array`` with the generated values from
+              ``instance._function``.
         """
         # Setup
         instance = AnonymizedFaker()
-        instance.null_transformer = Mock()
-        instance.null_transformer.models_missing_values.return_value = False
         instance.data_length = 3
         function = Mock()
         function.side_effect = ['a', 'b', 'c']
 
         instance._function = function
-        instance.null_transformer.reverse_transform.return_value = np.array(['a', 'b', 'c'])
 
         # Run
         result = instance._reverse_transform(None)
 
         # Assert
-        expected_null_call = np.array(['a', 'b', 'c'])
-        null_call = instance.null_transformer.reverse_transform.call_args_list[0][0][0]
-        np.testing.assert_array_equal(null_call, expected_null_call)
         assert function.call_args_list == [call(), call(), call()]
         np.testing.assert_array_equal(result, np.array(['a', 'b', 'c']))
-
-    def test__reverse_transform_models_missing_values(self):
-        """Test the ``_reverse_transform`` method.
-
-        Validate that the ``_reverse_transform`` method uses the ``instance._function``
-        to generate values within the range of the ``instance.data_length``.
-
-        Setup:
-            - Mock the instance of ``AnonymizedFaker``.
-            - Mock the ``instance.null_transformer.reverse_transform`` return value.
-
-        Input:
-            - ``pd.DataFrame`` with a column ``is_null`` that contains numeric values.
-
-        Output:
-            - The mocked return value of the ``null_transformer.reverse_transform``.
-
-        Side Effects:
-            - The ``instance._function`` has been called ``instance.data_length`` with
-              the ``function_args`` as keyword args.
-        """
-        # Setup
-        columns_data = pd.DataFrame({
-            'is_null': [0, 1, 0]
-        })
-        instance = Mock()
-        instance.null_transformer.reverse_transform.return_value = pd.Series([
-            'a',
-            np.nan,
-            'c',
-        ])
-        instance.get_output_columns.return_value = ['is_null']
-        function = Mock()
-        function.side_effect = [1, 2, 3]
-        instance._function = function
-        instance.data_length = 3
-        instance.function_kwargs = {
-            'type': 'a'
-        }
-
-        # Run
-        output = AnonymizedFaker._reverse_transform(instance, columns_data)
-
-        # Assert
-        expected_output = pd.Series([
-            'a',
-            np.nan,
-            'c'
-        ])
-        pd.testing.assert_series_equal(expected_output, output)
-
-        expected_call = np.array([
-            [1, 0],
-            [2, 1],
-            [3, 0]
-        ])
-        called_arg = instance.null_transformer.reverse_transform.call_args[0][0]
-        np.testing.assert_array_equal(expected_call, called_arg)
-
-        expected_function_calls = [call(), call(), call()]
-        assert function.call_args_list == expected_function_calls
 
     def test__reverse_transform_not_enough_unique_values(self):
         """Test the ``_reverse_transform`` method.
@@ -576,18 +405,14 @@ class TestAnonymizedFaker:
         arguments.
         """
         # Setup
-        instance = AnonymizedFaker('credit_card', 'credit_card_full', model_missing_values=True)
+        instance = AnonymizedFaker('credit_card', 'credit_card_full')
 
         # Run
         res = repr(instance)
 
         # Assert
-        expected_res = (
-            "AnonymizedFaker(provider_name='credit_card', function_name='credit_card_full', "
-            'model_missing_values=True)'
-        )
-
-        assert res == expected_res
+        expected = "AnonymizedFaker(provider_name='credit_card', function_name='credit_card_full')"
+        assert res == expected
 
 
 class TestPseudoAnonymizedFaker:
@@ -627,8 +452,6 @@ class TestPseudoAnonymizedFaker:
             - the ``instance.function_name`` is ``lexify``.
             - the ``instance.locales`` is ``None``.
             - the ``instance.function_kwargs`` is an empty ``dict``.
-            - the ``instance.missing_value_replacement`` is ``None``.
-            - the ``instance.model_missing_values`` is ``False``
             - ``check_provider_function`` has been called once with ``BaseProvider`` and
               ``lexify``.
             - the ``instance._function`` is ``instance.faker.lexify``.
@@ -644,8 +467,6 @@ class TestPseudoAnonymizedFaker:
         assert instance.provider_name == 'BaseProvider'
         assert instance.function_name == 'lexify'
         assert instance.function_kwargs == {}
-        assert instance.missing_value_replacement is None
-        assert not instance.model_missing_values
         assert instance.locales is None
         assert mock_faker.Faker.called_once_with(None)
 
@@ -669,8 +490,6 @@ class TestPseudoAnonymizedFaker:
             - the ``instance.function_name`` is ``credit_card_full``.
             - the ``instance.locales`` is ``['en_US', 'fr_FR']``.
             - the ``instance.function_kwargs`` is ``{'type': 'visa'}``.
-            - the ``instance.missing_value_replacement`` is ``None``.
-            - the ``instance.model_missing_values`` is ``False``
             - ``check_provider_function`` has been called once with ``CreditCard`` and
               ``credit_card_full``.
             - the ``instance._function`` is ``instance.faker.credit_card_full``.
@@ -692,8 +511,6 @@ class TestPseudoAnonymizedFaker:
         assert instance.provider_name == 'credit_card'
         assert instance.function_name == 'credit_card_full'
         assert instance.function_kwargs == {'type': 'visa'}
-        assert instance.missing_value_replacement is None
-        assert not instance.model_missing_values
         assert instance.locales == ['en_US', 'fr_FR']
         assert mock_faker.Faker.called_once_with(['en_US', 'fr_FR'])
 
