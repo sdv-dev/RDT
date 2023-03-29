0186727b14de0a6a00133040389241706aa830a8
Frances Hartwell <frances.hartwell@alteryx.com>
2023-03-29 16:08:39 -0400

    add version addon discovery

--- rdt/__init__.py
+++ rdt/__init__.py
@@ -7,6 +7,9 @@ __author__ = 'DataCebo, Inc.'
 __email__ = 'info@sdv.dev'
 __version__ = '1.3.1.dev0'
 
+import warnings
+from importlib.metadata import entry_points as get_entry_points
+
 import numpy as np
 import pandas as pd
 
@@ -83,3 +86,27 @@ def get_demo(num_rows=5):
         'age': age,
         'dollars_spent': dollars_spent
     }), ignore_index=True)
+
+
+def _add_version():
+    try:
+        entry_points = get_entry_points(name='version', group='rdt_modules')
+    except TypeError:
+        entry_points = [
+            entry_point for entry_point in get_entry_points().get('rdt_modules', [])
+            if entry_point.name == 'version'
+        ]
+
+    for entry_point in entry_points:
+        try:
+            module = entry_point.load()
+        except Exception:
+            msg = f'Failed to load "{entry_point.name}" from "{entry_point.module}". '
+            warnings.warn(msg)
+            continue
+
+        if 'version' not in globals():
+            globals()['version'] = module
+
+
+_add_version()
