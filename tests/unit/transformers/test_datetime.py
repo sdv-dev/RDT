07c31f9764bc55478ae6ce72269403641fb27328
Frances Hartwell <frances.hartwell@alteryx.com>
2023-06-29 15:01:19 -0400

    actually fix test

--- tests/unit/transformers/test_datetime.py
+++ tests/unit/transformers/test_datetime.py
@@ -434,7 +434,7 @@ class TestUnixTimestampEncoder:
         """Test the ``_reverse_transform`` method returns the correct datetime format with nans."""
         # Setup
         ute = UnixTimestampEncoder()
-        ute.datetime_format = '%b %-d, %Y'
+        ute.datetime_format = '%b %d, %Y'
         transformed = np.array([1.5778368e+18, 1.5805152e+18, np.nan])
         ute._dtype = 'object'
         ute.null_transformer = NullTransformer('mean')
@@ -443,7 +443,7 @@ class TestUnixTimestampEncoder:
         output = ute._reverse_transform(transformed)
 
         # Assert
-        expected = pd.Series(['Jan 1, 2020', 'Feb 1, 2020', np.nan])
+        expected = pd.Series(['Jan 01, 2020', 'Feb 01, 2020', np.nan])
         pd.testing.assert_series_equal(output, expected)
 
     def test__reverse_transform_only_nans(self):
