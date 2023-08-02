74f20aceb48f36ca314f66b6622324153600e603
Andrew Montanez <andrew@sdv.dev>
2023-08-02 15:55:19 -0500

    Bump version: 1.6.1 → 1.6.2.dev0

--- setup.py
+++ setup.py
@@ -139,6 +139,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/RDT',
-    version='1.6.1',
+    version='1.6.2.dev0',
     zip_safe=False,
 )
