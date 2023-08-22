2bfa2e29d312f3055df100a9c6e712685a528d8d
Andrew Montanez <andrew@sdv.dev>
2023-08-22 14:31:14 -0500

    Bump version: 1.7.0 → 1.7.1.dev0

--- setup.py
+++ setup.py
@@ -139,6 +139,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/RDT',
-    version='1.7.0',
+    version='1.7.1.dev0',
     zip_safe=False,
 )
