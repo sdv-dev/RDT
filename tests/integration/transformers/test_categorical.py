d81a557a908b85698819fec7ba5c896706260c44
Felipe <fealho@gmail.com>
2023-10-18 11:49:46 -0400

    Changed temporary file for fixture

--- tests/integration/transformers/test_categorical.py
+++ tests/integration/transformers/test_categorical.py
@@ -1,5 +1,4 @@
 import pickle
-import tempfile
 import warnings
 from io import BytesIO
 
@@ -426,7 +425,7 @@ def test_one_hot_numerical_nans():
     pd.testing.assert_frame_equal(reverse, data)
 
 
-def test_one_hot_doesnt_warn():
+def test_one_hot_doesnt_warn(tmp_path):
     """Ensure OneHotEncoder doesn't warn when saving and loading GH#616."""
     # Setup
     data = pd.DataFrame({'column_name': [1.0, 2.0, np.nan, 2.0, 3.0, np.nan, 3.0]})
@@ -434,11 +433,11 @@ def test_one_hot_doesnt_warn():
 
     # Run
     ohe.fit(data, 'column_name')
-    # Create a temporary file and dump the transformer
-    tmp = tempfile.NamedTemporaryFile()
-    pickle.dump(ohe, tmp)
-    tmp.seek(0)
-    ohe_loaded = pickle.load(tmp)
+    tmp = tmp_path / 'ohe.pkl'
+    with open(tmp, 'wb') as tmp:
+        pickle.dump(ohe, tmp)
+    with open(tmp.name, 'rb') as tmp:
+        ohe_loaded = pickle.load(tmp)
 
     # Assert
     with warnings.catch_warnings():
