519c217f9e5b414f3cadca1be8703ac89c0cfacd
lajohn4747 <john@datacebo.com>
2023-10-31 11:03:13 -0500

    Bump version: 1.8.0.dev2 → 1.8.0

--- setup.py
+++ setup.py
@@ -138,6 +138,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/RDT',
-    version='1.8.0.dev2',
+    version='1.8.0',
     zip_safe=False,
 )
