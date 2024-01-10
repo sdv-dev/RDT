f6086180d3ebe5bdf7ecb819cd0f2fc5a077d8ab
Andrew Montanez <andrew@sdv.dev>
2024-01-10 15:20:46 -0600

    Bump version: 1.9.1.dev0 → 1.9.1.dev1

--- setup.py
+++ setup.py
@@ -138,6 +138,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sdv-dev/RDT',
-    version='1.9.1.dev0',
+    version='1.9.1.dev1',
     zip_safe=False,
 )
