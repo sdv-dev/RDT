a3f794295eaf23482f314881e164170908efe2a4
Felipe <fealho@gmail.com>
2025-05-07 07:09:36 -0700

    Fix OverflowError

--- rdt/transformers/utils.py
+++ rdt/transformers/utils.py
@@ -436,7 +436,7 @@ def _safe_parse_datetime(value, warn=False):
 
         return dt
 
-    except (ValueError, TypeError, AttributeError):
+    except (ValueError, TypeError, AttributeError, OverflowError):
         return None
 
 
