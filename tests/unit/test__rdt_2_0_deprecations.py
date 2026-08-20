1732865df4d987f95d0a893f419f598fa989cd0d
R-Palazzo <romainpalazzo@gmail.com>
2026-08-20 14:38:52 +0100

    fix unit test python 3.9

--- tests/unit/test__rdt_2_0_deprecations.py
+++ tests/unit/test__rdt_2_0_deprecations.py
@@ -47,9 +47,7 @@ def test_deprecated_parameters(class_, method, parameter):
     """Test that deprecated parameters raise an error."""
     # Setup
     instance = class_()
-    expected_message = (
-        f"{class_.__name__}.{method}() got an unexpected keyword argument '{parameter}'"
-    )
+    expected_message = f"{method}() got an unexpected keyword argument '{parameter}'"
 
     # Run and Assert
     with pytest.raises(TypeError, match=re.escape(expected_message)):
